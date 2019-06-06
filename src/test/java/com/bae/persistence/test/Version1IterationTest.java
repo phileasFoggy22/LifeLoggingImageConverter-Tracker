0b642e80e43afb5a8f742a50bfc3ea161cf1cb26
phileasFoggy22 <36856831+phileasFoggy22@users.noreply.github.com>
2019-06-06 22:00:42 +0100

    RG Refactor - Red Only - Iteration 1 V1

--- src/test/java/com/bae/persistence/test/Version1IterationTest.java
+++ src/test/java/com/bae/persistence/test/Version1IterationTest.java
@@ -1,36 +1,35 @@
 package com.bae.persistence.test;
 
 import org.junit.Before;
-import org.junit.Test;
 
 public class Version1IterationTest {
 	@Before
 	public void start() {
 	}
 
-	@Test
-	public void addNewActivity() {
-		assert (false);
-	}
-
-	@Test
-	public void updateActivity() {
-		assert (false);
-	}
-
-	@Test
-	public void deleteActivity() {
-		assert (false);
-	}
-
-	@Test
-	public void getAllActivities() {
-		assert (false);
-	}
-
-	@Test
-	public void getAllActivitiesbyCategory() {
-		assert (false);
-	}
+	// @Test
+	// public void addNewActivity() {
+	// assert (false);
+	// }
+	//
+	// @Test
+	// public void updateActivity() {
+	// assert (false);
+	// }
+	//
+	// @Test
+	// public void deleteActivity() {
+	// assert (false);
+	// }
+	//
+	// @Test
+	// public void getAllActivities() {
+	// assert (false);
+	// }
+	//
+	// @Test
+	// public void getAllActivitiesbyCategory() {
+	// assert (false);
+	// }
 
 }
